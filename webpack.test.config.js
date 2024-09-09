13f06996514a3be0c7752a56ff7b83c53394b5cc
Arun S <arun.s@aot-technologies.com>
2024-09-09 21:57:49 +0530

    FWF-3425 [Feature] : Updated the settings to accomodate formiojs changes. Package updation

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -3,12 +3,18 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.jsx?$/,
+				test: /\.(js|jsx|ts|tsx)$/,
 				exclude: /node_modules/,
 				loader: 'babel-loader',
 				options: {
 					presets: [
-						['es2015', { modules: false }],
+						[
+							'es2015',
+							{ modules: false },
+							'@babel/preset-env',
+							'@babel/preset-react',
+							'@babel/preset-typescript',
+						],
 						'react',
 						'stage-2',
 					],
